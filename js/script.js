acb6d54ae6b7a23bbb1e434da6a4db24c46e81f2
RKNyra <rk.nyra@gmail.com>
2019-08-18 15:47:18 +0300

    update javascript to equate gender value to respective Akan Name

--- js/script.js
+++ js/script.js
@@ -1,50 +1,12 @@
-// function calculateAkanName() {
-//     alert("Done!");
-
-// }
-// function generateAkanName() {
-//     alert("Form Submitted");
-// }
-// function generateAkanName(e) {
-//     document.getElementById("reveal-result").innerHTML = "Your Akan Name is ...!!";
-//     console.log(e)
-// }
-
-// window.addEventListener('load', function() {
-
-
-// })
-
-// let form = document.querySelector("form");
-
-// console.log(form);
-
-// form.addEventListener('submit', function(e) {
-//     document.getElementById("reveal-result").innerHTML = "Your Akan Name is ...!!";
-//     e.preventDefault()
-// // })
-// function generateAkanName() {
-//     document.getElementById("reveal-result").innerHTML = "Your Akan Name is ...";
-// }
 let akanForm = document.getElementById("akanForm");
 akanForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("Form Submitted");
-    //alert("complete");
-    let result = document.getElementById("reveal-result").innerHTML = "Your Akan Name is ..."
 
     gender = document.getElementById("gender").value
-        //to get the value of gender selected by the user
     dateOfBirth = document.getElementById("date").value
-        //to get the value of date input by the user
-
-    // dateOfBirth = dateOfBirth.split("")
-    // console.log(dateOfBirth);
 
     dateOfBirth = new Date(dateOfBirth);
-    //dateOfBirth = parseInt(dateOfBirth) - this will only convert the first part of the string and not the entire date
-    //console.log(dateOfBirth)
-    //to convert value of date(which is a string) to numbers
+
     let yearOfBirth = dateOfBirth.getFullYear();
     let birthMonth = dateOfBirth.getMonth() + 1;
     let dayOfBirth = dateOfBirth.getDate();
@@ -55,33 +17,71 @@ akanForm.addEventListener("submit", (e) => {
     DD = dayOfBirth
 
     dayOfTheWeek = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
-    //console.log(dayOfTheWeek); this brings day of the week in numbers which can also have decimals.
 
     dayOfTheWeek = parseInt(dayOfTheWeek);
 
+    let akanName;
+
+    femaleAkanNames = ["Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
+    maleAkanNames = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+
     let bornDay;
 
-    if (dayOfTheWeek == 1) {
+    if (dayOfTheWeek == 0 && gender == "female") {
+        bornDay = "Saturday";
+        akanName = femaleAkanNames[0];
+    } else if (dayOfTheWeek == 0 && gender == "male") {
+        bornDay = "Saturday";
+        akanName = maleAkanNames[0];
+
+
+    } else if (dayOfTheWeek == 1 && gender == "female") {
         bornDay = "Sunday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 2) {
+        akanName = femaleAkanNames[1];
+    } else if (dayOfTheWeek == 1 && gender == "male") {
+        bornDay = "Sunday";
+        akanName = maleAkanNames[1];
+
+
+    } else if (dayOfTheWeek == 2 && gender == "female") {
         bornDay = "Monday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 3) {
+        akanName = femaleAkanNames[2];
+    } else if (dayOfTheWeek == 2 && gender == "male") {
+        bornDay = "Monday";
+        akanName = maleAkanNames[2];
+
+
+    } else if (dayOfTheWeek == 3 && gender == "female") {
         bornDay = "Tuesday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 4) {
+        akanName = femaleAkanNames[3];
+    } else if (dayOfTheWeek == 3 && gender == "male") {
+        bornDay = "Tuesday";
+        akanName = maleAkanNames[3];
+
+
+    } else if (dayOfTheWeek == 4 && gender == "female") {
         bornDay = "Wednesday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 5) {
+        akanName = femaleAkanNames[4];
+    } else if (dayOfTheWeek == 4 && gender == "male") {
+        bornDay = "Wednesday";
+        akanName = maleAkanNames[4];
+
+
+    } else if (dayOfTheWeek == 5 && gender == "female") {
         bornDay = "Thursday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 6) {
+        akanName = femaleAkanNames[5];
+    } else if (dayOfTheWeek == 5 && gender == "male") {
+        bornDay = "Thursday";
+        akanName = maleAkanNames[5];
+
+
+    } else if (dayOfTheWeek == 6 && gender == "female") {
         bornDay = "Friday";
-        console.log(bornDay);
-    } else if (dayOfTheWeek == 7) {
-        bornDay = "Saturday"
+        akanName = femaleAkanNames[6];
+    } else if (dayOfTheWeek == 6 && gender == "male") {
+        bornDay = "Friday";
+        akanName = maleAkanNames[6];
     }
 
-
+    let result = document.getElementById("reveal-result").innerHTML = "You were born on a " + bornDay + " so your akan name is " + akanName + "!"
 });
